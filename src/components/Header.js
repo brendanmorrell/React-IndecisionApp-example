2c55bd5aa65e99fbec867cf605e533af10beb6d2
Brendan Morrell <brendanmorrell@gmail.com>
2017-10-19 15:15:18 -0600

    set up a way to break style components up. if you create a partial you use an underscore at the beginning of the file name as opposed to full stylesheets with regular names. created a header partial and used BEM naming convention to style elements in the header component with __header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Header = (props) => (
-  <div>
-    <h1>{props.title}</h1>
-    {props.subtitle && <h2>{props.subtitle}</h2>}
+  <div className="header"/*name is deprecated in react, so have to use className to make it a class as opposed to a prop*/>
+    <h1 className="header__title">{props.title}</h1>
+    {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
   </div>
 );
 Header.defaultProps = {
