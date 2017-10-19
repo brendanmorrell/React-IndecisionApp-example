3d7ac277efcc12a5a986f2af47bbc34f1912c1b3
Brendan Morrell <brendanmorrell@gmail.com>
2017-10-19 14:33:38 -0600

    yarned sass-loader for webpack config loading sass (or) scss and node-sass which actually takes sass or scss code and converts to regular css. you only need to config sass-loader, as node reuns behind the scenes

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,11 @@ module.exports = {
       test: /\.js$/,
       exclude: /node_modules/,
     }, {
-      test:/\.css$/,
+      test:/\.scss$/,
       use: [
         'style-loader',
-        'css-loader'
+        'css-loader',
+        'sass-loader'
       ]
     }]
   },
