a5d9c9272948ae831dfb69d07f04ca26bfad510e
Brendan Morrell <brendanmorrell@gmail.com>
2017-12-02 09:45:50 -0700

    changed webpack to return the statement

--- webpack.config.js
+++ webpack.config.js
@@ -6,28 +6,30 @@ const webpack = require('webpack');
 
 
 module.exports = (env) => {
-const isProduction = env === 'production';
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/,
-    }, {
-      test:/\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public')
-  }
+  const isProduction = env === 'production';
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      }, {
+        test:/\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public')
+    }
+  };
 };
